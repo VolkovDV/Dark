ac449984066e18c1b77c94a187adcd71b9643d71
Dmitrii <volkovdmvd@gmail.com>
2019-04-26 23:06:49 +0300

    First commit

--- main.py
+++ main.py
@@ -2,6 +2,18 @@ from kivy.app import App
 from kivy.uix.button import Button
 
 
+class Ball:
+    color: str
+
+    def move(self):
+        pass
+
+    def push(self):
+        pass
+
+    def take(self):
+        pass
+
 class TestApp(App):
     def build(self):
         return Button(text='Hello World')
